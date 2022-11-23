bb187ea440ecd474ac29190528f02bcca3d6cb03
KahhorovSh04 <shakhzodbekkakhkhorov04@gmail.com>
2022-11-24 01:52:08 +0800

    packages and configs

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className='container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10'>
+      <header className='App-header'>
+        <p className='text-3xl font-bold underline text-red-700'>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
