031f553813611d95f97977f71116dd23a8966a3b
KahhorovSh04 <shakhzodbekkakhkhorov04@gmail.com>
2022-12-14 21:23:19 +0800

    To-Do list design

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,8 +5,37 @@ const Section = () => {
     <section>
       <article>
         <h1>You should complete these today:</h1>
+        <div className='table'>
+          <div className='row'>
+            <div>1</div>
+            <div>Read a book</div>
+            <div>1 hr</div>
+            <div>NA</div>
+            <div>
+              <input type='checkbox' />
+            </div>
+          </div>
+          <div className='row done'>
+            <div>2</div>
+            <div>Do Pomas project</div>
+            <div>4 hrs</div>
+            <div>NA</div>
+            <div>
+              <input type='checkbox' checked readOnly />
+            </div>
+          </div>
+          <div className='row done'>
+            <div>3</div>
+            <div>Take a shower</div>
+            <div>NA</div>
+            <div>10 pm</div>
+            <div>
+              <input type='checkbox' checked readOnly />
+            </div>
+          </div>
+        </div>
       </article>
-      <article>Article 2</article>
+      <article></article>
     </section>
   )
 }
